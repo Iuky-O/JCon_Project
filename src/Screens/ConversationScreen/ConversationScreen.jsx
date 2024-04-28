130cb062b6c7eeb3eebdb7790b8159449dd63bf8
Iuky.O <iumyfarias03@gmail.com>
2024-04-28 14:25:35 -0300

    botton tab

---
+++ src/Screens/ConversationScreen/ConversationScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ConversationScreen() {
+  return (
+    <View>
+      <Text>ConversationScreen</Text>
+    </View>
+  )
+}
---
+++ src/Utils/Colors.js
@@ -0,0 +1,12 @@
+export default{
+    AZUL_MARINHO: '#03045E',
+    MALTE: '#023E8A',
+    AZURE: '#0077B6',
+    AZUL_PACIFICO: '#0096C7',
+    AZUL_IRIS: '#00B4D8',
+    TURQUESA: '#48CAE4',
+    AZUL_CELESTE: '#90E0EF',
+    NEVASCA_AZUL: '#ADE8F4',
+    TURQUESA_CLARO: '#CAF0F8',
+    BRANCO: '#FFFFFF'
+}
