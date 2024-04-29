2f06842ebfab864ca6053153be12975114d0adfe
leh826 <dev.rocha826@gmail.com>
2024-04-29 15:53:02 -0300

    acrescentado header e uma img

--- src/Screens/JobScreen/JobScreen.jsx
+++ src/Screens/JobScreen/JobScreen.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Colors from '../../Utils/Colors';
 
 export default function JobScreen() {
   return (
-    <View>
-      <Text>JobScreen</Text>
+    <View style={styles.header}> 
+      <Text style={styles.text}>JobScreen</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  header: {
+    height: 90,
+    backgroundColor: Colors.MALTE,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    marginTop: 35,
+    fontSize:30,
+  }
+});
