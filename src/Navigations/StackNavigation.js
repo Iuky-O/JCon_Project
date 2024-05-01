22c7c4069772eac194a68fe2d6529308836692f8
EduardoTrikk <154011898+EduardoTrikk@users.noreply.github.com>
2024-05-01 12:40:09 -0300

    Update StackNavigation.js
    foi adicionado a stack navigation

--- src/Navigations/StackNavigation.js
+++ src/Navigations/StackNavigation.js
@@ -1,13 +1,20 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import HomeScreen from '../Screens/HomeScreen/HomeScreen';
+import HomeScreen from '../Screens/HomeScreen/HomeScreen'
+import ConversationsScreen from '../Screens/ConversationScreen/ConversationScreen'
+import ChatScreen from '../Screens/ChatScreen/ChatScreen'
+import TabNavigation from '../Navigations/TabNavigation'
 
 const Stack = createStackNavigator();
 export default function StackNavigation() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{
+      headerShown: false,}}>
+        <Stack.Screen name='Tab' component={TabNavigation}/>
         <Stack.Screen name='Home' component={HomeScreen}/>
+        <Stack.Screen name='Conversations' component={ConversationsScreen}/>
+        <Stack.Screen name='Chat' component={ChatScreen}/>
+
     </Stack.Navigator>
   )
 }
