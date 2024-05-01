c81e01a35db2cbc42c5327fdf52dc14debd4de51
leh826 <dev.rocha826@gmail.com>
2024-05-01 20:09:42 -0300

    JobScreen, ProfileSreen

--- src/Screens/ProfileScreen/ProfileScreen.jsx
+++ src/Screens/ProfileScreen/ProfileScreen.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 
 export default function ProfileScreen() {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.header}>
+        <Ionicons style={styles.iconBack}
+          name="chevron-back"
+          size={40}
+          color="write"
+          onPress={() => { }}
+        />
+        <Image style={styles.imgLogo}
+          source={require('../../../assets/images/logo-azul.jpg')}
+          resizeMode='contain'
+        />
+      </View>
+      
+    </SafeAreaView>
   )
 }
---
+++ src/Screens/ProfileScreen/style.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native"
+import Colors from '../../Utils/Colors';
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    height: 110,
+    backgroundColor: Colors.AZUL_MARINHO,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  iconBack:{
+    marginTop:35, 
+    color:Colors.BRANCO,
+  },
+  imgLogo:{
+    width: 50, 
+    height: 70, 
+    marginTop:35,
+  },
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: Colors.TURQUESA_CLARO,
+    alignItems: "center",
+    marginTop: 100,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+});
