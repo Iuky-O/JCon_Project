3423db9a51e721263bb5a77a48904746e6dd605c
leh826 <dev.rocha826@gmail.com>
2024-05-01 12:19:11 -0300

    styles-jobscreen

--- src/Screens/JobScreen/JobScreen.jsx
+++ src/Screens/JobScreen/JobScreen.jsx
@@ -2,14 +2,13 @@ import { View, Text} from 'react-native'
 import React from 'react'
 import styles from '../../Utils/style';
 
-
 export default function JobScreen() {
   return (
     <><View style={styles.header}>
-      <Text style={styles.text}>JOBCREEN</Text>
+      <Text style={styles.text}>JobScreen</Text>
     </View><View style={styles.container}>
 
-      </View></>
+  </View></>
 
   )
 }
