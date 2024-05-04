850ef4323ead835defa7e76517bb17e6a52d3e31
Letícia <117243523+leh826@users.noreply.github.com>
2024-05-04 16:12:37 -0300

    alterado ProfileScreen.jsx

--- src/Screens/ProfileScreen/ProfileScreen.jsx
+++ src/Screens/ProfileScreen/ProfileScreen.jsx
@@ -1,5 +1,5 @@
 
-import { View, Image, Text, SafeAreaView, ScrollView, Animated, Dimensions, ImageBackground } from 'react-native'
+import { View, Image, Text, SafeAreaView, ScrollView, Animated, Dimensions, ImageBackground, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { Ionicons } from 'react-native-vector-icons';
 import styles from './style';
@@ -8,8 +8,9 @@ export default function ProfileScreen() {
   const [scrollY, setSrollY] = useState(new Animated.Value(0));
 
   return (
+
     <SafeAreaView style={{ height: Dimensions.get('window').height }}>
-      <ScrollView 
+      <ScrollView
         scrollEventThrottle={16}
         onScroll={Animated.event([{
           nativeEvent: {
@@ -17,29 +18,36 @@ export default function ProfileScreen() {
           },
         }],
           { useNativeDriver: false })}
-      > 
-      <View style={styles.header}>
-        <Ionicons style={styles.iconBack}
-          name="chevron-back"
-          size={40}
-          color="write"
-          onPress={() => { }}
-        />
-        <Image style={styles.imgLogo}
-          source={require('../../../assets/images/logo-azul.jpg')}
-          resizeMode='contain'
-        />
-      </View>
-      <View style={styles.containerImg}>
-        <ImageBackground
-        source={require('../../../assets/images/profile.jpg')}
-        style={styles.imgProfile}
-      ></ImageBackground>
-      </View>                   
-      <View style={styles.container} >
+      >
+        <View style={styles.header}>
+          <Ionicons style={styles.iconBack}
+            name="chevron-back"
+            size={40}
+            color="write"
+            onPress={() => { }}
+          />
+          <Image style={styles.imgLogo}
+            source={require('../../../assets/images/logo-azul.jpg')}
+            resizeMode='contain'
+          />
         </View>
-      </ScrollView >
+        <View style={styles.containerImg}>
+          <Image
+            source={require('../../../assets/images/profile.jpg')}
+          />
+        </View>
+        <View style={styles.container}>
+          <Text >
+            <Text style={styles.textTitle}>JANE COOPER </Text>
+          </Text>
+          <View style={styles.Button}>
+            <Text style={styles.buttomText} onPress={() => Alert.alert('Bubmissão enviada!')}> Oferecer Proposta </Text>
+          </View>
+          <Text>Serviços</Text>
+          <Text style={styles.lineSeparator}></Text>
+        </View>
 
+      </ScrollView>
     </SafeAreaView>
   )
 }
