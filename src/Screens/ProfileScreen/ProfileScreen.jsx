f0d88498e35cf328ac5d4b47a7c0d3ac0bd94c27
leh826 <dev.rocha826@gmail.com>
2024-05-07 10:42:33 -0300

    profile finalizada, alterado icon-back

--- src/Screens/ProfileScreen/ProfileScreen.jsx
+++ src/Screens/ProfileScreen/ProfileScreen.jsx
@@ -1,11 +1,14 @@
 
-import { View, Image, Text, SafeAreaView, ScrollView, Animated, Dimensions, ImageBackground, Alert } from 'react-native'
+import { View, Image, Text, SafeAreaView, ScrollView, Animated, Dimensions, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { Ionicons } from 'react-native-vector-icons';
 import styles from './style';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 export default function ProfileScreen() {
   const [scrollY, setSrollY] = useState(new Animated.Value(0));
+  const navigation = useNavigation();
 
   return (
 
@@ -20,31 +23,68 @@ export default function ProfileScreen() {
           { useNativeDriver: false })}
       >
         <View style={styles.header}>
+          <TouchableOpacity
+          onPress = {() => navigation.goBack()}>
           <Ionicons style={styles.iconBack}
             name="chevron-back"
             size={40}
             color="write"
             onPress={() => { }}
-          />
+          />          
+          </TouchableOpacity>
           <Image style={styles.imgLogo}
             source={require('../../../assets/images/logo-azul.jpg')}
             resizeMode='contain'
           />
         </View>
+
         <View style={styles.containerImg}>
           <Image
             source={require('../../../assets/images/profile.jpg')}
           />
         </View>
+
         <View style={styles.container}>
-          <Text >
-            <Text style={styles.textTitle}>JANE COOPER </Text>
+
+          <Text>
+            <Text style={styles.textTitle}>Jane Cooper </Text>
           </Text>
+
           <View style={styles.Button}>
-            <Text style={styles.buttomText} onPress={() => Alert.alert('Bubmissão enviada!')}> Oferecer Proposta </Text>
+            <Text style={styles.buttomText} onPress={() => Alert.alert('Proposta feita!')}> Oferecer Proposta </Text>
           </View>
-          <Text>Serviços</Text>
+
+          <Text style={styles.textSubtitle}>SERVIÇOS</Text>
           <Text style={styles.lineSeparator}></Text>
+
+          <View style={styles.textInfo}>
+            <Text>Design ➖ ➖ </Text>
+            <Text>Programação ➖ ➖ </Text>
+            <Text>UX </Text>
+          </View>
+
+          <Text style={styles.textSubtitle}>SOBRE MIM</Text>
+          <Text style={styles.lineSeparator}></Text>
+
+          <Text style={{ margin: 20, }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima est sit iure id totam suscipit maiores. Id fugiat culpa quisquam molestiae esse repudiandae. Adipisci cumque possimus, consectetur perferendis similique itaque.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam porro saepe, soluta necessitatibus libero quo inventore adipisci. Doloribus, minima quasi harum eius possimus suscipit quaerat dolor minus laudantium ratione libero?
+          </Text>
+
+          <Text style={styles.textSubtitle}>POSTAGENS</Text>
+
+          <View style={styles.post}>
+            <Image style={styles.postImage}
+              source={require('../../../assets/images/postagen1.jpg')}
+            />
+            <Image style={styles.postImage}
+              source={require('../../../assets/images/postagem2.jpg')}
+            />
+            <Image style={styles.postImage}
+              source={require('../../../assets/images/postagem3.jpg')}
+            />
+          </View>
+
         </View>
 
       </ScrollView>
--- src/Screens/ProfileScreen/style.js
+++ src/Screens/ProfileScreen/style.js
@@ -21,10 +21,11 @@ const styles = StyleSheet.create({
     marginTop: 35,
   },
   container: {
-   flex:1,
+   //flex:1,
     backgroundColor: Colors.TURQUESA_CLARO,
     height:700,
     borderRadius:30,
+    alignItems:"center",
 
   },
   lineSeparator: {
@@ -39,29 +40,58 @@ const styles = StyleSheet.create({
   },
   textTitle:{
     fontSize:40,
-    justifyContent:"center",
     color: Colors.AZUL_MARINHO
+  },
+  textSubtitle:{
+    fontSize:20,
+    marginTop:30,
+    fontWeight: 'bold',
+    color:Colors.MALTE,
+  },
+  textInfo:{
+    flexDirection:"row",
+    margin:15,
+    color:Colors.MALTE,
   },
   Button:{
     backgroundColor: Colors.MALTE,
+    marginTop:30,
     maxWidth: 300,
-    width: 200,
+    width: 300,
     height: 50,
     borderRadius: 20,
-    alignItems: 'center',
+  alignItems: "center",
   },
   lineSeparator: {
     height: 1,
-    width: "100%",
+    width: "90%",
     backgroundColor: Colors.AZUL_IRIS,
   },
   buttomText:{
-    margin:10,
+    marginTop:10,
     color: Colors.BRANCO,
     fontStyle:"bold",
     fontSize:20,
   },
-
+  traco:{
+    margin:10,
+    maxWidth: 50,
+    width: 50,
+    height: 50,
+    borderRadius: 100, 
+    backgroundColor: Colors.MALTE,
+  },
+  post:{
+    flexDirection: 'row',
+    margin: 0,
+    width: "100%",
+    padding: 15,
+  },
+  postImage:{
+    margin:5,
+    height:140,
+    width:130,
+  },
 });
 export default styles
 
