375aa0456cdbc68da31fa56231c31bf9dd209bfc
EduardoTrikk <154011898+EduardoTrikk@users.noreply.github.com>
2024-05-12 19:37:52 -0300

    atualizando a tela login

--- src/Screens/LoginScreen/LoginScreen.jsx
+++ src/Screens/LoginScreen/LoginScreen.jsx
@@ -1,58 +1,86 @@
-import { View, Text, TextInput, Button} from 'react-native'
+import { View, TouchableOpacity, Text, TextInput, Button, Touchable} from 'react-native'
 import React, {useState} from 'react'
 import { auth } from '../../Firebase/firebaseConfig';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigation } from '@react-navigation/native';
-
+import styles from './styles'; // Importa os estilos do arquivo styles.js
+import Colors from '../../Utils/Colors'; // Importa as cores do arquivo Colors.js
 
 export default function LoginScreen() {
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
-    const navigation = useNavigation(); 
+    const [userEmail, setUserEmail] = useState(''); // Estado para o email do usuário
+    const [userPassword, setUserPassword] = useState(''); // Estado para a senha do usuário
+    const navigation = useNavigation(); // Navegação do React Navigation
 
     function newUser(){
-      navigation.navigate('Register');
+      navigation.navigate('Register'); // Função para navegar para a tela de registro
     }
 
     function userLogin(){
-        signInWithEmailAndPassword(auth, userEmail, userPassword)
-        .then((userCredential) => {
+        signInWithEmailAndPassword(auth, userEmail, userPassword) // Função para fazer login com email e senha
+        .then((userCredential) => { // Se o login for bem-sucedido
             const user = userCredential.user;
-            alert("Login efetuado com sucesso!");
-            console.log(user);
+            alert("Login efetuado com sucesso!"); // Exibe uma mensagem de sucesso
+            console.log(user); // Loga o usuário no console
             
-            navigation.navigate('Home');
-            
+            navigation.navigate('Home'); // Navega para a tela Home após o login
+
         })
-        .catch((error) => {
+        .catch((error) => { // Se houver algum erro no login
             const errorCode = error.code;
             const errorMessage = error.message;
 
-            alert(errorMessage);
+            alert(errorMessage); // Exibe uma mensagem de erro
         })
     }
-
+    
   return (
-    <View style={{alignItems:'center', marginTop: 100, gap: 16}}>
-      <Text style={{fontSize: 30}}>Login</Text>
-      <TextInput placeholder='Email' 
-                 keyboardType='email-address'
-                 autoCapitalize='none' 
-                 value={userEmail} 
-                 onChangeText={setUserEmail}
-      />
-      <TextInput placeholder='Senha' 
-                 autoCapitalize='none'
-                 secureTextEntry
-                 value={userPassword} 
-                 onChangeText={setUserPassword}
-      />
-      <Button title='Logar' onPress={userLogin}/>
+    <View> 
+      <Text style={styles.titulo}>Login</Text> 
+      <Text style={styles.subtitulo}>Entre com sua conta</Text> 
 
-      <View style={{}}>
-        <Text>Redefinir senha</Text>
-        <Text onPress={newUser}>Cadastrar-se</Text>
+    <View style={styles.corpo}>
+    {/* Não estva pegando os icones, não sei pq.
+      <MaterialIcons
+            name="email"
+            size={30}
+            style={styles.iconemail} // Ícone para o email
+          />
+          */}
+      <TextInput      
+          style={styles.email} // Campo de entrada para o email
+          placeholder='Email'
+          placeholderTextColor={Colors.AZUL_PACIFICO} 
+          keyboardType='email-address'
+          autoCapitalize='none' 
+          value={userEmail} 
+          onChangeText={setUserEmail}
+      />
+          {/* Não estva pegando os icones, não sei pq.
+          <MaterialIcons
+            name="vpn-key"
+            size={30}
+            style={styles.iconsenha} // Ícone para a senha
+          />
+          */}
+      <TextInput 
+          style={styles.senha} // Campo de entrada para a senha
+          placeholder='Senha' 
+          placeholderTextColor={Colors.AZUL_PACIFICO} 
+          autoCapitalize='none'
+          secureTextEntry
+          value={userPassword} 
+          onChangeText={setUserPassword}
+      />
+                                        {/* falta o link para o esqueceu a senha*/}
+      <Text style={styles.redefinirsenha}>Esqueceu a senha?</Text> 
+      <TouchableOpacity style={styles.botao} onPress={userLogin}> 
+      <Text style={styles.botaotxt}>Login</Text>
+    </TouchableOpacity>
+      <View> 
+                                                                                                  {/* falta o link para o cadastro*/}
+        <Text style={styles.naotemconta}>Não tem uma conta?<Text style={styles.cadastrar} onPress={newUser}> Cadastrar</Text></Text>             
       </View>
+    </View>
     </View>
   )
 }
