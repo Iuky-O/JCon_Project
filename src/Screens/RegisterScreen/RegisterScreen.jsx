c4fdb744d870115c5d42bae7dc1abb3f57c55caa
EduardoTrikk <154011898+EduardoTrikk@users.noreply.github.com>
2024-05-19 22:06:21 -0300

    Tela de registro feita

--- src/Screens/RegisterScreen/RegisterScreen.jsx
+++ src/Screens/RegisterScreen/RegisterScreen.jsx
@@ -1,32 +1,33 @@
-import { View, Text, TextInput, TouchableOpacity, ScrollView, SafeAreaView} from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
-
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../Firebase/firebaseConfig';
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import styles from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { CheckBox } from 'react-native-elements';
+import { Image } from 'react-native-elements';
 
 export default function RegisterScreen() {
-
   const [userName, setUserName] = useState('');
   const [userTel, setUserTel] = useState('');
   const [userCpf_Cnpj, setUserCpf_Cnpj] = useState('');
   const [userEndereco, setUserEndereco] = useState('');
   const [userPhoto, setUserPhoto] = useState('');
   const [userType, setUserType] = useState('');
-
+  const [isChecked, setIsChecked] = useState(false);
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
   const [userRePassword, setUserRePassword] = useState('');
-
+  const [currentStep, setCurrentStep] = useState(1);
   const navigation = useNavigation();
 
   async function addUserData() {
     try {
       const db = getFirestore();
-      const usersCollection = collection(db, "usuarios");
+      const usersCollection = collection(db, 'usuarios');
 
       await addDoc(usersCollection, {
         Name: userName,
@@ -39,12 +40,9 @@ export default function RegisterScreen() {
         Password: userPassword,
       });
 
-      console.log("Dados do usuário adicionados com sucesso!");
-
+      console.log('Dados do usuário adicionados com sucesso!');
     } catch (error) {
-
-      console.error("Erro ao adicionar dados do usuário:", error);
-
+      console.error('Erro ao adicionar dados do usuário:', error);
     }
   }
 
@@ -60,24 +58,19 @@ export default function RegisterScreen() {
       addUserData();
       createUserWithEmailAndPassword(auth, userEmail, userPassword)
         .then((userCredential) => {
-
           const user = userCredential.user;
-          alert('O usuário ' + userName + ' foi criado. Faça login!')
+          alert('O usuário ' + userName + ' foi criado. Faça login!');
           navigation.navigate('Login');
-
         })
         .catch((error) => {
           const errorMessage = error.message;
           alert(errorMessage);
-
           navigation.navigate('Login');
         });
     }
-
   }
 
   const pickImage = async () => {
-
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -93,90 +86,143 @@ export default function RegisterScreen() {
     }
   };
 
-  return (
-    <SafeAreaView>
-       
-    <ScrollView>
-        
-      <View style={styles.corpo}>
-        <Text style={styles.Title}>Cadastro</Text>
+  const toggleCheckBox = () => {
+    setIsChecked(!isChecked);
+  };
+  const NextStep = () => {
+    if (isChecked) {
+      setCurrentStep(2);
+    } else {
+      alert('Você precisa aceitar os termos de uso e política de privacidade para continuar.');
+    }
+  };
 
-        <TextInput
-          style={styles.Input}
-          placeholder='Nome'
-          autoCapitalize='none'
-          value={userName}
-          onChangeText={setUserName}
+  const renderStep1 = () => (
+    <View>
+        <View style={styles.logoContainerStep1}>
+          <Image source={require('../../../assets/images/logo.png')} style={styles.logoStep1}/>
+        </View>
+      <Text style={styles.textostep1}>Qual seu objetivo?</Text> 
+      <TouchableOpacity onPress={NextStep} setUserType='prestador' style={styles.icons}>
+        <Icon name="briefcase" size={50} color={'#023E8A'} />
+        <Text style={styles.textprestar}>Prestar Serviços</Text>
+      </TouchableOpacity>   
+      <TouchableOpacity onPress={NextStep} setUserType='contratante' style={styles.icons}>
+        <Icon name="handshake" size={50} color={'#023E8A'} />
+        <Text style={styles.textcontratar}>Contratar Serviços</Text>
+      </TouchableOpacity>
+      <View style={styles.checkboxContainer}>
+        <CheckBox      
+          checked={isChecked}
+          onPress={toggleCheckBox}
+          containerStyle={{ backgroundColor: 'transparent', borderWidth: 0 }}
         />
-        <TextInput
-          style={styles.Input}
-          placeholder='Telefone'
-          autoCapitalize='none'
-          value={userTel}
-          onChangeText={setUserTel}
-        />
-        <TextInput
-          style={styles.Input}
-          placeholder='CPF ou CNPJ'
-          autoCapitalize='none'
-          value={userCpf_Cnpj}
-          onChangeText={setUserCpf_Cnpj}
-        />
-        <TextInput
-          style={styles.Input}
-          placeholder='Endereço'
-          autoCapitalize='none'
-          value={userEndereco}
-          onChangeText={setUserEndereco}
-        />
-        <TouchableOpacity style={styles.button}
-          onPress={pickImage}
-          value={userPhoto}
-          onChangeText={setUserPhoto}>
-          <Text style={styles.buttonTxt}>FOTO DE PERFIL</Text>
+        <Text style={styles.checkboxText}>Eu aceito os termos de uso e política de privacidade.</Text>
+      </View>
+    </View>
+  );
+
+  const renderStep2 = () => (
+    <View>
+        <View style={styles.logoContainerStep2}>
+          <Image source={require('../../../assets/images/register-step2.png')} style={styles.logoStep2}/>
+        </View>
+    <Text style={styles.textostep2}>Conte mais sobre você!</Text>
+      <TextInput
+        style={styles.Input}
+        placeholder='Nome'
+        autoCapitalize='none'
+        value={userName}
+        onChangeText={setUserName}
+      />
+      <TextInput
+        style={styles.Input}
+        placeholder='Telefone'
+        autoCapitalize='none'
+        value={userTel}
+        onChangeText={setUserTel}
+      />
+      <TextInput
+        style={styles.Input}
+        placeholder='CPF ou CNPJ'
+        autoCapitalize='none'
+        value={userCpf_Cnpj}
+        onChangeText={setUserCpf_Cnpj}
+      />
+      <TextInput
+        style={styles.Input}
+        placeholder='Endereço'
+        autoCapitalize='none'
+        value={userEndereco}
+        onChangeText={setUserEndereco}
+      />
+      <TouchableOpacity style={styles.buttoncamera} onPress={pickImage}>
+        <Icon name="camera" size={50} color={'#023E8A'} />
+        <Text style={styles.cameratxt}>Inserir Foto</Text>
+      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonVoltar} onPress={() => setCurrentStep(1)}>
+          <Text style={styles.buttonTxt}>VOLTAR</Text>
         </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => setCurrentStep(3)}>
+          <Text style={styles.buttonTxt}>PRÓXIMO</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 
-        <TextInput
-          style={styles.Input}
-          placeholder='Escolha seu tipo de usuário'
-          autoCapitalize='none'
-          value={userType}
-          onChangeText={setUserType}
-        />
+  const renderStep3 = () => (
+    <View>
+      <View style={styles.logoContainerStep3}>
+          <Image source={require('../../../assets/images/register-step3.png')} style={styles.logoStep3}/>
+      </View>
+    <Text style={styles.textostep3}>Suas informações para login</Text>
+      <TextInput
+        style={styles.Input}
+        placeholder='Informe seu email'
+        keyboardType='email-address'
+        autoCapitalize='none'
+       
 
-        <TextInput
-          style={styles.Input}
-          placeholder='Informe seu email'
-          keyboardType='email-address'
-          autoCapitalize='none'
-          value={userEmail}
-          onChangeText={setUserEmail}
-        />
-        <TextInput
-          style={styles.Input}
-          placeholder='Informe uma senha'
-          autoCapitalize='none'
-          secureTextEntry
-          value={userPassword}
-          onChangeText={setUserPassword}
-        />
-        <TextInput
-          style={styles.Input}
-          placeholder='Confirme a senha'
-          autoCapitalize='none'
-          secureTextEntry
-          value={userRePassword}
-          onChangeText={setUserRePassword}
-        />
-
+        value={userEmail}
+        onChangeText={setUserEmail}
+      />
+      <TextInput
+        style={styles.Input}
+        placeholder='Informe uma senha'
+        autoCapitalize='none'
+        secureTextEntry
+        value={userPassword}
+        onChangeText={setUserPassword}
+      />
+      <TextInput
+        style={styles.Input}
+        placeholder='Confirme a senha'
+        autoCapitalize='none'
+        secureTextEntry
+        value={userRePassword}
+        onChangeText={setUserRePassword}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonVoltar} onPress={() => setCurrentStep(2)}>
+          <Text style={styles.buttonTxt}>VOLTAR</Text>
+        </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={newUserLogin}>
           <Text style={styles.buttonTxt}>CADASTRAR</Text>
         </TouchableOpacity>
-      </View> 
-    </ScrollView>
-    
-  </SafeAreaView>
-       
- 
-  )
+      </View>
+    </View>
+  );
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.corpo}>
+          {currentStep === 1 && renderStep1()}
+          {currentStep === 2 && renderStep2()}
+          {currentStep === 3 && renderStep3()}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
--- src/Screens/RegisterScreen/styles.js
+++ src/Screens/RegisterScreen/styles.js
@@ -1,44 +1,155 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from "react-native";
 import Colors from '../../Utils/Colors';
 
 const styles = StyleSheet.create({
-    corpo: {
-        alignContent: "center",
-        alignItems: "center",
-        marginTop: 50,
-        gap: 16,
-    },
-    Title: {
-        fontSize: 30,
-        color: Colors.MALTE,
-    },
-    Input: {
-        color: Colors.AZUL_PACIFICO,
-        backgroundColor: Colors.BRANCO,
-        borderRadius: 10,
-        borderColor: Colors.AZUL_IRIS,
-        borderWidth: 1,
-        marginTop: 5,
-        marginLeft:5,
-        width: '90%',
-        height:35,
-        paddingHorizontal: 10,
-
-    },
-    button: {
-        backgroundColor: Colors.MALTE,
-        padding: 10,
-        borderRadius: 10,
-        width: 150,
-    },
-    buttonTxt: {
-        color: Colors.BRANCO,
-        textAlign: "center",
-        fontSize: 12,
-    },
-    Icon:{
-        width:20,
-        height:20,
-    }
-})
+    logoContainerStep1: {   
+    display: 'flex',
+    alignItems: 'center', 
+    marginTop: 50, 
+  },
+  logoStep1: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 150,
+    height: 150,
+  },
+  logoContainerStep2: {   
+    display: 'flex',
+    alignItems: 'center', 
+    marginTop: 30, 
+  },
+  logoStep2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 250,
+    height: 250,
+  },
+  logoContainerStep3: {   
+    display: 'flex',
+    alignItems: 'center',  
+  },
+  logoStep3: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    height: 300,
+  },
+  corpo: {
+    flex: 1, // Ocupa o espaço inteiro disponível
+    justifyContent: "center",
+    paddingHorizontal: 20, // Padding lateral para que os elementos não encostem nas bordas
+  },
+  textostep1: {
+    textAlign: "center",
+    color: Colors.MALTE,
+    marginBottom: 40,
+    fontSize: 30,
+    marginTop: 20,
+  },
+  textostep2: {
+    textAlign: "center",
+    color: Colors.MALTE,
+    fontSize: 30,
+    marginTop: 20,
+  },
+  textostep3: {
+    textAlign: "center",
+    color: Colors.MALTE,
+    fontSize: 30,
+  },
+  icons: {
+    marginBottom: 20,
+    borderColor: Colors.AZUL_IRIS,
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    justifyContent: 'center',
+    backgroundColor: Colors.BRANCO,
+  },
+  textprestar: {
+    color: Colors.AZUL_IRIS,
+    alignSelf: 'center',
+    fontSize: 18,
+    position: 'absolute',
+    color: '#808080',
+  },
+  textcontratar: {
+    color: Colors.AZUL_IRIS,
+    alignSelf: 'center',
+    alignItems: 'center',
+    fontSize: 18,
+    position: 'absolute',
+    color: '#808080',
+  },
+  checkboxContainer: {
+    marginTop: 50,
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  checkboxText: {
+    width: '80%',
+  },
+  Input: {
+    color: Colors.AZUL_PACIFICO,
+    backgroundColor: Colors.BRANCO,
+    fontSize: 18,
+    borderRadius: 10,
+    borderColor: Colors.AZUL_IRIS,
+    borderWidth: 1,
+    marginVertical: 10, // Margem vertical para espaçar entre os inputs
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    width: '100%', // Ocupa toda a largura disponível do contêiner pai
+  },
+  button: {
+    backgroundColor: Colors.MALTE,
+    padding: 10,
+    borderRadius: 10,
+    width: 150,
+    alignSelf: 'center',
+    marginVertical: 5,
+  },
+  buttonVoltar: {
+    backgroundColor: Colors.AZURE,
+    padding: 10,
+    borderRadius: 10,
+    width: 150,
+    alignSelf: 'center',
+    marginVertical: 5,
+  },
+  buttonContainer: {
+    flex: 1, 
+    flexDirection: 'row', 
+    gap: 30, 
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  buttoncamera: {
+    marginTop: 20,
+    marginBottom: 20,
+    borderColor: Colors.AZUL_IRIS,
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    justifyContent: 'center',
+    backgroundColor: Colors.BRANCO,
+  },
+  cameratxt: {
+    color: Colors.AZUL_IRIS,
+    alignSelf: 'center',
+    fontSize: 18,
+    position: 'absolute',
+    color: '#808080',
+  },
+  buttonTxt: {
+    color: Colors.BRANCO,
+    textAlign: "center",
+    fontSize: 12,
+  },
+  Icon: {
+    width: 20,
+    height: 20,
+  },
+});
 export default styles;
